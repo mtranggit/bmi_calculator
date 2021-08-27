2a2bd120033b0b23950c1784f5ad02ace40dd7f3
Michael Trang <michael.trang@hotmail.com>
2021-08-28 07:35:41 +1000

    use material dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -8,14 +8,9 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
+      theme: ThemeData.dark().copyWith(
         primaryColor: Color(0xFF0A0E21),
         scaffoldBackgroundColor: Color(0xFF0A0E21),
-        textTheme: TextTheme(
-          bodyText2: TextStyle(
-            color: Color(0xFFFFFFFF),
-          ),
-        ),
       ),
       home: InputPage(),
     );
