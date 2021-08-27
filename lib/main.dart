fdefc5c0c47e0558368c158b979d0e501ee11874
Michael Trang <michael.trang@hotmail.com>
2021-08-28 07:33:59 +1000

    create an app them

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,15 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+        textTheme: TextTheme(
+          bodyText2: TextStyle(
+            color: Color(0xFFFFFFFF),
+          ),
+        ),
+      ),
       home: InputPage(),
     );
   }
